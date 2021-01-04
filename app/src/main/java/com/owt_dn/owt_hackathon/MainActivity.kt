084bc84804a14784858aa8ba86bf1f20afef5ded
Man Nguyen <mnguyen@openwt.com>
2021-01-04 17:12:20 +0700

    init

---
+++ app/src/main/java/com/owt_dn/owt_hackathon/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.owt_dn.owt_hackathon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
