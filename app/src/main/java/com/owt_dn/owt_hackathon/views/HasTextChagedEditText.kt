2c5182d2ca547970d16d033a01053c1bd0d5efda
Man Nguyen <mnguyen@openwt.com>
2021-01-05 06:10:58 +0700

    use custom EditText

---
+++ app/src/main/java/com/owt_dn/owt_hackathon/views/HasTextChagedEditText.kt
@@ -0,0 +1,32 @@
+package com.owt_dn.owt_hackathon.views
+
+import android.content.Context
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.AttributeSet
+import androidx.appcompat.widget.AppCompatEditText
+
+
+class HasTextChagedEditText : AppCompatEditText {
+
+    constructor(context: Context) : super(context)
+
+    constructor(context: Context, attributeSet: AttributeSet) : super(context, attributeSet)
+
+    fun onTextChanged(onTextChanged: (String) -> Unit) {
+        addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                // Not implemented
+            }
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                onTextChanged.invoke(p0.toString())
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+                // Not implemented
+            }
+        })
+    }
+
+}
